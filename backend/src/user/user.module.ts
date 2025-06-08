57597bf65f665de56968fc97da9a2cea04f54583
Yordanis Tejeda <yordanisrodriguez855@protonmail.com>
2025-06-07 22:30:14 -0400

    Refactor: remove spec file for the moment and update files for better connection with the database

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,13 +1,12 @@
 import {Module} from '@nestjs/common';
 import {UserService} from './user.service';
 import {UserController} from './user.controller';
-import {PrismaModule} from "../prisma/prisma.module";
 import {UserRepository} from "./user.repository";
 
 @Module({
     controllers: [UserController],
     providers: [UserService, UserRepository],
-    imports: [PrismaModule]
+    imports: []
 })
 export class UserModule {
 }
