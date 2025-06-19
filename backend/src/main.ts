fe421c7b106707a6805511918449c46e1a630f21
Yordanis Tejeda <yordanisrodriguez855@protonmail.com>
2025-06-18 22:47:39 -0400

    Feat: add global prefix 'api' for all routes except 'health'

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,16 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app/app.module';
+import {RequestMethod} from "@nestjs/common";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.setGlobalPrefix('api', {
+        exclude: [{path: 'health', method: RequestMethod.GET},
+            {path: '/', method: RequestMethod.GET}]
+    });
+
+
     await app.listen(process.env.PORT ?? 3000);
 }
 
