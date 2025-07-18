435d90ad38f65d0870d900554ef4fa0f1a7c5744
Yordanis Tejeda <yordanisrodriguez855@protonmail.com>
2025-07-17 22:07:23 -0400

    feat: add the ability of capture other error of PrismaORM

--- backend/src/utils/prisma-handler.ts
+++ backend/src/utils/prisma-handler.ts
@@ -1,16 +1,19 @@
 import {PrismaClientKnownRequestError} from '@prisma/client/runtime/library'
 import {ConflictException, NotFoundException} from "@nestjs/common";
 
-export default function prismaHandler(error : any){
+export default function prismaHandler(error: any) {
     if (error instanceof PrismaClientKnownRequestError) {
         switch (error.code) {
             case 'P2002' :
                 throw new ConflictException("Already exist");
+            case 'P2003' :
+                throw new ConflictException("Can't delete it"); //TODO ver cual sera la excepcion correcta
             case 'P2025':
                 throw new NotFoundException("Doesn't exist");
             default:
                 throw new Error(`A database error occurred ${error.message}`);
         }
     }
+    // TODO Lo del error del validation error
     throw error;
 }
