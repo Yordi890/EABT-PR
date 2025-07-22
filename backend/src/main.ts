fdee53947dd6435b25ea8dd71e38952404027bde
Yordanis Tejeda <yordanisrodriguez855@protonmail.com>
2025-07-21 22:16:58 -0400

    refactor: fixed some eslint problems detected

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app/app.module';
 import { ConsoleLogger, RequestMethod, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule, {
     logger: new ConsoleLogger({
       compact: true,
@@ -11,9 +11,9 @@ async function bootstrap() {
     }),
   });
 
-  const configService = app.get(ConfigService);
+  const configService: ConfigService = app.get(ConfigService);
 
-  const PORT = configService.get('PORT');
+  const PORT: number | undefined = configService.get('PORT');
 
   app.useGlobalPipes(
     new ValidationPipe({
