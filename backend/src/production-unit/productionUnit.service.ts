e9d79d1f513d4e64a2f8d27f174a57ebe37d1883
Yordanis Tejeda <yordanisrodriguez855@protonmail.com>
2025-08-02 13:51:19 -0400

    feat(logs): added logs for all methods in production-unit module

--- backend/src/production-unit/productionUnit.service.ts
+++ backend/src/production-unit/productionUnit.service.ts
@@ -3,21 +3,49 @@ import { ProductionUnitRepository } from './productionUnit.repository';
 import { ProductionUnitModel } from '../../generated/prisma/models/ProductionUnit';
 import { ProductionUnitDto } from './dto/productionUnit.dto';
 import prismaHandler from '../utils/prisma-handler';
+import { LoggerService } from '../logger/logger.service';
 
 @Injectable()
 export class ProductionUnitService {
   constructor(
     private readonly productionUnitRepository: ProductionUnitRepository,
+    private readonly logger: LoggerService,
   ) {}
 
   async listAllProductionUnit(): Promise<ProductionUnitModel[]> {
-    return await this.productionUnitRepository.retrieveAllProductionUnit();
+    this.logger.log('Intentando listar todas las unidades de producción');
+
+    try {
+      const productionUnits: ProductionUnitModel[] =
+        await this.productionUnitRepository.retrieveAllProductionUnit();
+
+      this.logger.log('Unidades de producción listadas exitosamente');
+
+      return productionUnits;
+    } catch (error) {
+      this.logger.error('Error al listar unidades de producción');
+      prismaHandler(error);
+      return [];
+    }
   }
 
   async addProductionUnit(productionUnit: ProductionUnitDto): Promise<void> {
+    this.logger.log(
+      `Intentando crear unidad de producción con nombre: ${productionUnit.name}`,
+    );
     try {
-      await this.productionUnitRepository.insertProductionUnit(productionUnit);
+      const productionUnitCreated: ProductionUnitModel =
+        await this.productionUnitRepository.insertProductionUnit(
+          productionUnit,
+        );
+      this.logger.log(
+        'Unidad de producción creada exitosamente',
+        productionUnitCreated,
+      );
     } catch (error) {
+      this.logger.error(
+        `Error al crear unidad de producción con nombre: ${productionUnit.name}`,
+      );
       prismaHandler(error);
     }
   }
@@ -26,20 +54,38 @@ export class ProductionUnitService {
     name: string,
     newProductionUnit: ProductionUnitDto,
   ): Promise<void> {
+    this.logger.log(
+      `Intentando modificar unidad de producción con nombre: ${name}`,
+    );
     try {
       await this.productionUnitRepository.saveProductionUnit(
         name,
         newProductionUnit,
       );
+      this.logger.log(
+        `Unidad de producción modificada exitosamente con nombre: ${name}`,
+      );
     } catch (error) {
+      this.logger.error(
+        `Error al modificar unidad de producción con nombre: ${name}`,
+      );
       prismaHandler(error);
     }
   }
 
   async deleteProductionUnit(name: string): Promise<void> {
+    this.logger.log(
+      `Intentando eliminar unidad de producción con nombre: ${name}`,
+    );
     try {
       await this.productionUnitRepository.removeProductionUnit(name);
+      this.logger.log(
+        `Unidad de producción eliminada exitosamente con nombre: ${name}`,
+      );
     } catch (error) {
+      this.logger.error(
+        `Error al eliminar unidad de producción con nombre: ${name}`,
+      );
       prismaHandler(error);
     }
   }
