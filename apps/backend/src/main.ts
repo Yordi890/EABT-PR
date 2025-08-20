f49863e201aec42bfc42f5066187855c9ac24f66
Yordanis Tejeda <yordanisrodriguez855@protonmail.com>
2025-08-19 21:27:36 -0400

    feat: add new packages and configuring swagger
    - Installed new packages for the future.
    - Setting up swaggerModule

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ConsoleLogger, RequestMethod, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule, {
@@ -13,6 +14,14 @@ async function bootstrap(): Promise<void> {
   const PORT: number | undefined = configService.get('PORT');
 
   // console.log("Este es el hash de turbo " + configService.get('TURBO_HASH'));
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Swagger')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
