723e5c489c32b14ebe451a1ef3d898b2689ea8ba
Yordanis Tejeda <yordanisrodriguez855@protonmail.com>
2025-09-01 22:24:34 -0400

    Pendiente a revision, abortando estructura monorepo

--- packages/database/dist/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { PrismaClient } from "../generated/prisma/client";
-export * from "../generated/prisma/models";
-export * from "../generated/prisma/client";
-export * from "../generated/prisma/enums";
-export declare const prismaClient: import("../generated/prisma/internal/class").PrismaClient<never, import("../generated/prisma/internal/prismaNamespace").GlobalOmitConfig | undefined, import("@prisma/client/runtime/library").DefaultArgs>;
-//# sourceMappingURL=index.d.ts.map
