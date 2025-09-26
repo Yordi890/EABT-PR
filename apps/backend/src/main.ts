7e3664f601b5ac00cf08c1fbae4986e9508f3dac
Yordanis Tejeda <yordanisrodriguez855@protonmail.com>
2025-09-25 23:27:29 -0400

    Configure Swagger to run only in development based on NODE_ENV

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,24 +1,20 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
-import { ConsoleLogger, RequestMethod, ValidationPipe, VersioningType } from '@nestjs/common';
+import { AppModule } from './app/app.module.js';
+import { RequestMethod, ValidationPipe, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
-import * as fs from 'fs';
-import * as path from 'path';
-import * as yaml from 'yaml';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule, {
-    logger: new ConsoleLogger(),
-  });
+  const app = await NestFactory.create(AppModule);
+
+  const configService: ConfigService = app.get(ConfigService);
+
+  app.enableShutdownHooks();
 
   app.enableVersioning({
     type: VersioningType.URI,
     defaultVersion: '1',
   });
-  const configService: ConfigService = app.get(ConfigService);
-
-  const PORT: number | undefined = configService.get('PORT');
 
   app.setGlobalPrefix('api', {
     exclude: [
@@ -27,23 +23,32 @@ async function bootstrap(): Promise<void> {
     ],
   });
 
-  const config = new DocumentBuilder()
-    .addGlobalResponse({
-      status: 500,
-      description: 'Internal server error',
-    })
-    .setTitle('NestJS Swagger')
-    .setDescription('API description')
-    .setVersion('1.0')
-    .build();
-  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
+  if (configService.get<string>('NODE_ENV') === 'development') {
+    const { stringify } = await import('yaml');
+    const { resolve, dirname } = await import('path');
+    const { writeFileSync } = await import('fs');
+    const { fileURLToPath } = await import('url');
 
-  fs.writeFileSync(
-    path.resolve(__dirname, '../../../../packages/openapi/openapi.yaml'),
-    yaml.stringify(document),
-    'utf8',
-  );
+    const document: OpenAPIObject = SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder()
+        .addGlobalResponse({
+          status: 500,
+          description: 'Internal server error',
+        })
+        .setTitle('NestJS Swagger')
+        .setDescription('API description')
+        .setVersion('1.0')
+        .build(),
+    );
+    SwaggerModule.setup('swagger', app, document);
+
+    writeFileSync(
+      resolve(dirname(fileURLToPath(import.meta.url)), '../../../../packages/openapi/openapi.yaml'),
+      stringify(document),
+      'utf8',
+    );
+  }
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -53,7 +58,7 @@ async function bootstrap(): Promise<void> {
     }),
   );
 
-  await app.listen(PORT ?? 3000);
+  await app.listen(configService.get('PORT') ?? 3000);
 }
 
 bootstrap();
