a9042299d3f38214a3451fa3886c53d7831e6e2a
Yordanis Tejeda <yordanisrodriguez855@protonmail.com>
2025-09-25 21:50:27 -0400

    feat(databases): Remove Rust engine and add Prisma adapter, closes #1

--- apps/backend/src/prisma/prisma.module.ts
+++ apps/backend/src/prisma/prisma.module.ts
@@ -1,9 +1,9 @@
 import { Global, Module } from '@nestjs/common';
-import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaService } from './prisma.service.js';
 
 @Global()
 @Module({
-  providers: [PrismaClient],
-  exports: [PrismaClient],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class PrismaModule {}
---
+++ apps/backend/src/prisma/prisma.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, OnApplicationShutdown, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '../../generated/prisma/client.js';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnApplicationShutdown {
+  constructor() {
+    super({
+      adapter: new PrismaPg({
+        connectionString: process.env.DATABASE_URL!,
+      }),
+    });
+  }
+
+  async onModuleInit(): Promise<void> {
+    await this.$connect();
+  }
+
+  async onApplicationShutdown(signal?: string) {
+    await this.$disconnect();
+  }
+}
