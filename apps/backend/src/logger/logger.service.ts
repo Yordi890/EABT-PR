47a7e023f662bbbef23f34ba1ee28317fc3fe4b8
Yordanis Tejeda <yordanisrodriguez855@protonmail.com>
2025-10-04 22:22:53 -0400

    refactor: recreated the logs module

--- apps/backend/src/logger/logger.service.ts
+++ apps/backend/src/logger/logger.service.ts
@@ -1,23 +1,50 @@
-// src/logger/logger.service.ts
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import { createLogger, format, transports, Logger as WinstonLogger } from 'winston';
+import { inspect } from 'util';
 
 @Injectable()
 export class LoggerService {
-  private readonly logger = new Logger();
+  private readonly winstonLogger: WinstonLogger;
 
-  log(message: string, context?: string) {
-    this.logger.log(message, context);
+  constructor() {
+    this.winstonLogger = createLogger({
+      level: 'info',
+      format: format.combine(
+        format.timestamp(),
+        format.printf(({ level, message, timestamp }) => {
+          return JSON.stringify({
+            timestamp,
+            level: level.toUpperCase(),
+            message,
+          });
+        }),
+      ),
+      transports: [new transports.Console()],
+    });
+  }
+
+  private formatMessage(message: string, obj?: any): string {
+    if (!obj) return message;
+    const objString = inspect(obj, { depth: null, showHidden: false }).replace(/\n/g, '');
+    return `${message} ${objString}`;
+  }
+
+  log(message: string, obj?: any, context?: string) {
+    const formattedMessage = this.formatMessage(message, obj);
+    this.winstonLogger.info(formattedMessage);
   }
 
   error(message: string, trace?: string, context?: string) {
-    this.logger.error(message, trace, context);
+    this.winstonLogger.error(this.formatMessage(message, trace));
   }
 
-  warn(message: string, context?: string) {
-    this.logger.warn(message, context);
+  warn(message: string, obj?: any, context?: string) {
+    const formattedMessage = this.formatMessage(message, obj);
+    this.winstonLogger.warn(formattedMessage);
   }
 
-  debug(message: string, context?: string) {
-    this.logger.debug(message, context);
+  debug(message: string, obj?: any, context?: string) {
+    const formattedMessage = this.formatMessage(message, obj);
+    this.winstonLogger.debug(formattedMessage);
   }
 }
