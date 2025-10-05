3daac2871adf4115a6abea114e3d28f256fcc9a0
Yordanis Tejeda <yordanisrodriguez855@protonmail.com>
2025-10-04 22:21:28 -0400

    refactor: Updated the authentication module to use the new user.service implementation

--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -1,18 +1,20 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { UserService } from '../users/user.service';
-import { LoginDto } from './dto/loginDto';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { LoginDto } from './dto/loginDto.js';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
+import { GenericService } from '../generic-crud/generic.service.js';
+import { UserModel } from '../../generated/prisma/models/User.js';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private readonly userService: UserService,
+    @Inject('USER_SERVICE')
+    private readonly userService: GenericService<UserModel, any, string>,
     private readonly jwtService: JwtService,
   ) {}
 
   async login({ userName, password }: LoginDto) {
-    const user = await this.userService.getUserByUsername(userName);
+    const user = await this.userService.findByField('userName', userName);
 
     if (!user || !(await bcrypt.compare(password, user.password))) {
       throw new UnauthorizedException('Invalid credentials');
