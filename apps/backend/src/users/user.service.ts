e4620f018aaccf917e3bf8a5ef29b907c41be9ec
Yordanis Tejeda <yordanisrodriguez855@protonmail.com>
2025-10-04 22:39:38 -0400

    feat(user): extend GenericService to customize user creation behavior

---
+++ apps/backend/src/users/user.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { GenericService } from '../generic-crud/generic.service.js';
+import { UserModel } from '../../generated/prisma/models/User.js';
+import { UserDto } from './dto/user.dto.js';
+import { LoggerService } from '../logger/logger.service.js';
+import * as bcrypt from 'bcrypt';
+import { IGenericRepository } from '../generic-crud/interfaces/generic.repository.interface.js';
+import handlePrismaError from '../utils/prisma-handler.js';
+
+@Injectable()
+export class UserService extends GenericService<UserModel, UserDto, string> {
+  constructor(
+    @Inject('USER_REPOSITORY') repository: IGenericRepository<UserModel, UserDto, string>,
+    logger: LoggerService,
+  ) {
+    super(repository, logger);
+  }
+
+  // Sobrescribimos el método create para encriptar la contraseña
+  async create(item: UserDto): Promise<UserModel> {
+    item.password = await bcrypt.hash(item.password, 10);
+
+    try {
+      const createdItem = await this.repository.create(item);
+      this.logger.log('Elemento creado en usuarios', item);
+      return createdItem;
+    } catch (err) {
+      handlePrismaError(err);
+    }
+  }
+
+  // Puedes añadir métodos específicos para usuarios si es necesario
+  async getUserByUsername(username: string): Promise<UserModel | null> {
+    return this.repository.findByField('userName', username);
+  }
+}
