a87db467bfff8ae33bdfe1e0be24037700651566
Yordanis Tejeda <yordanisrodriguez855@protonmail.com>
2025-10-04 22:26:24 -0400

    feat(user): integrate generic module to create UserModule

--- apps/backend/src/users/user.module.ts
+++ apps/backend/src/users/user.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
+import { GenericModule } from '../generic-crud/generic.module.js';
+import { UserService } from './user.service.js';
+import { UserDto } from './dto/user.dto.js';
 
-@Module({})
+@Module({
+  imports: [
+    GenericModule.forRoot({
+      name: 'user',
+      routePrefix: 'users',
+      modelName: 'user',
+      idFieldName: 'identityCard',
+      dto: UserDto,
+      customService: UserService,
+    }),
+  ],
+  providers: [],
+  exports: [GenericModule],
+})
 export class UserModule {}
