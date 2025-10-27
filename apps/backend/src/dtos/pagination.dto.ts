a37ec165aa8d257bbeeaf17b3df04c3f7953bc93
Yordanis Tejeda <yordanisrodriguez855@protonmail.com>
2025-10-27 04:15:34 -0400

    feat: create paginationDto

---
+++ apps/backend/src/dtos/pagination.dto.ts
@@ -0,0 +1,7 @@
+import { IsInt, Min } from 'class-validator';
+
+export default class PaginationDto {
+  @IsInt({ message: 'El número de página debe ser un entero' })
+  @Min(1, { message: 'El número de página debe ser mayor o igual a 1' })
+  page: number; // Página actual (por defecto: 1)
+}
